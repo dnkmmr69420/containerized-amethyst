9ffaa10f1e3bbdb3514a3c240ba55c5ffc14cc20
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-29 02:59:12 -0500

    Update user-wide-install.sh

--- user-wide-install.sh
+++ user-wide-install.sh
@@ -6,6 +6,8 @@ rm -f ~/.local/bin/came-setup
 rm -f ~/.local/bin/create-came-container
 rm -f ~/.local/bin/reset-came-conf
 rm -f ~/.local/bin/reset-came-container
+rm -f ~/.local/bin/came-exec
+rm -f ~/.local/bin/enter-came
 
 echo "Downloading and installing came"
 wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/came
@@ -13,6 +15,8 @@ wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized
 wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/create-came-container
 wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/reset-came-conf
 wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/reset-came-container
+wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/came-exec
+wget -P ~/.local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/enter-came
 
 echo "Setting came scripts to executable"
 chmod a+x ~/.local/bin/came
@@ -20,5 +24,7 @@ chmod a+x ~/.local/bin/came-setup
 chmod a+x ~/.local/bin/create-came-container
 chmod a+x ~/.local/bin/reset-came-conf
 chmod a+x ~/.local/bin/reset-came-container
+chmod a+x ~/.local/bin/came-exec
+chmod a+x ~/.local/bin/enter-came
 
 echo "Finnished installing"
