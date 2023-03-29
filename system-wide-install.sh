dbcade8342d896e49bd53d4ca674cd8d54592c55
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-29 08:57:22 -0500

    Update system-wide-install.sh

--- system-wide-install.sh
+++ system-wide-install.sh
@@ -8,6 +8,7 @@ sudo rm -f /usr/local/bin/reset-came-conf
 sudo rm -f /usr/local/bin/reset-came-container
 sudo rm -f /usr/local/bin/came-exec
 sudo rm -f /usr/local/bin/enter-came
+sudo rm -f /usr/local/bin/stop-came
 
 sudo echo "Downloading and installing came"
 sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/came
@@ -17,6 +18,7 @@ sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/contai
 sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/reset-came-container
 sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/came-exec
 sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/enter-came
+sudo wget -P /usr/local/bin https://raw.githubusercontent.com/dnkmmr69420/containerized-amethyst/main/bin/stop-came
 
 sudo echo "Setting came scripts to executable"
 sudo chmod a+x /usr/local/bin/came
@@ -26,5 +28,6 @@ sudo chmod a+x /usr/local/bin/reset-came-conf
 sudo chmod a+x /usr/local/bin/reset-came-container
 sudo chmod a+x /usr/local/bin/came-exec
 sudo chmod a+x /usr/local/bin/enter-came
+sudo chmod a+x /usr/local/bin/stop-came
 
 sudo echo "Finnished installing"
