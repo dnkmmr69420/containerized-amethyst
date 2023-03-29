5cdc9a8440a1811b5709bf36cbf683f9b80d06c2
dnkmmr <104856296+dnkmmr69420@users.noreply.github.com>
2023-03-29 03:52:48 -0500

    Create system-wide-uninstall.sh

---
+++ system-wide-uninstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo echo "Uninstalling came"
+
+sudo rm -f /usr/local/bin/came
+sudo rm -f /usr/local/bin/came-setup
+sudo rm -f /usr/local/bin/create-came-container
+sudo rm -f /usr/local/bin/reset-came-conf
+sudo rm -f /usr/local/bin/reset-came-container
+sudo rm -f /usr/local/bin/came-exec
+sudo rm -f /usr/local/bin/enter-came
+
+sudo echo "Came is now uninstalled"
